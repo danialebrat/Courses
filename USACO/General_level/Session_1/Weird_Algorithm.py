1161c5e9a868fab1da608c219048ebc6b6166725
Danialebrat <Danial.ebrat@gmail.com>
2023-07-16 15:09:45 -0400

    editing Weird_Algorithm.py

--- USACO/General_level/Session_1/Weird_Algorithm.py
+++ USACO/General_level/Session_1/Weird_Algorithm.py
@@ -1,22 +1,14 @@
-def algorithm(n):
-    sequence = [n]
-
-    while n != 1:
-        if n % 2 == 0:
-            n = n // 2
-        else:
-            n = n * 3 + 1
-
-        sequence.append(n)
-
-    return sequence
-
 
 # Read the input
 n = int(input())
 
-# Call the algorithm function
-sequence = algorithm(n)
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+        print(n, end=" ")
+    else:
+        n = n * 3 + 1
+        print(n, end=" ")
 
-# Print the sequence
-print(" ".join(str(num) for num in sequence))
+
+
